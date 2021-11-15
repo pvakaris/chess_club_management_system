2f49299177a12b3814e3c50f39437c9db7500fac
ElisabethHalvorsen <elisabeth1999@sf-nett.no>
2021-11-15 11:43:40 +0000

    Create user models in models.py
    - Create Applicant model
    - Update requirements.txt to include; libgravatar, Faker and humanize

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from .users import Users
+
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
+from libgravatar import Gravatar
+
+class Applicant(models.Model):
+    """Applicants in their club."""
+
+    user_type = Users.APPLICANT
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+        validators=[RegexValidator(
+            regex=r'^@\w{3,}$',
+            message='Username must consist of @ followed by at least three alphanumericals'
+        )]
+    )
+    email = models.EmailField(unique=True, blank=False)
+    bio = models.CharField(max_length=520, blank=True)
+    chess_experience = models.IntegerField(blank=False)
+    personal_statement = models.CharField(max_length=10000, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    def gravatar(self, size=120):
+        """Return a URL to the user's gravatar."""
+        gravatar_object = Gravatar(self.email)
+        gravatar_url = gravatar_object.get_image(size=size, default='mp')
+        return gravatar_url
+
+    def mini_gravatar(self):
+        """Return a URL to a miniature version of the user's gravatar."""
+        return self.gravatar(size=60)
+    
+    class Meta:
+        """Model options."""
+
+        ordering = ['-created_at']
+
+
+
+
+
 
 # Create your models here.
---
+++ clubs/users.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Users(Enum):
+    CLUB_OWNER = 1
+    OFFICER = 2
+    MEMBER = 3
+    APPLICANT = 4
+    
