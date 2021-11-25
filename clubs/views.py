46ef744429301423400cff24866eeff0df05f231
ElisabethHalvorsen <elisabeth1999@sf-nett.no>
2021-11-25 20:29:33 +0000

    Create basic layout for showing users

--- clubs/views.py
+++ clubs/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+from .models import User, Member, Club
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.core.exceptions import ObjectDoesNotExist
+
 
 def home(request):
     return render(request, 'home.html')
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'user_list.html', {'users': users})
+
+def show_user(request, user_id):
+    try:
+        user = User.objects.get(id=user_id)
+    except ObjectDoesNotExist:
+        return redirect('user_list')
+    else:
+        return render(request, 'show_user.html', {'user': user})
--- system/urls.py
+++ system/urls.py
@@ -20,4 +20,7 @@ from clubs import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('users/', views.user_list, name='user_list'),
+    path('user/<int:user_id>', views.show_user, name='show_user'),
+
 ]
