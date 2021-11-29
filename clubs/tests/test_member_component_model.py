55956893c4129c7b78a060948b070fb2de37bcaf
Ojebo <ojebo.adoh@kcl.ac.uk>
2021-11-29 11:02:05 +0000

    Create initial tests for membership

---
+++ clubs/tests/test_member_component_model.py
@@ -0,0 +1,112 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+import clubs.user_types
+from clubs.models import *
+
+class MemberModelTestcase(TestCase):
+    """Component test for member, club and user since it needs the other two."""
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='johndoe@example.org',
+            first_name='John',
+            last_name='Doe',
+            password='password123',
+            bio='The quick brown fox jumps over the lazy dog.',
+            personal_statement='hhhh',
+            chess_experience=101,
+        )
+        self.club = Club.objects.create(
+            name="Club",
+            location="Location",
+            description="Description"
+        )
+        self.member = Member.objects.create(
+            user_type=UserTypes.APPLICANT,
+            current_user=self.user,
+            club_membership=self.club
+        )
+
+    def test_membership_is_valid(self):
+        try:
+            self.user.full_clean()
+        except (ValidationError):
+            self.fail('Test user should be valid')
+        try:
+            self.club.full_clean()
+        except (ValidationError):
+            self.fail('Test club should be valid')
+        self._assert_membership_is_valid()
+
+    """User type tests"""
+
+    def test_membership_type_may_be_member(self):
+        self.member.user_type=UserTypes.MEMBER
+        self._assert_membership_is_valid()
+
+    def test_membership_type_may_be_officer(self):
+        self.member.user_type=UserTypes.OFFICER
+        self._assert_membership_is_valid()
+
+    def test_membership_type_may_be_club_owner(self):
+        self.member.user_type=UserTypes.CLUB_OWNER
+        self._assert_membership_is_valid()
+
+    def test_membership_type_may_not_be_greater_than_4(self):
+        self.member.user_type=5
+        self._assert_membership_is_invalid()
+
+    def test_membership_type_may_not_be_less_than_1(self):
+        self.member.user_type=0
+        self._assert_membership_is_invalid()
+
+    def test_user_type_may_already_exist(self):
+        second_user = self._create_second_user()
+        second_club = self._create_second_club()
+        second_membership = self._create_second_member(second_user)
+        self.member.user_type = second_membership.user_type
+        self._assert_membership_is_valid()
+
+    """Current user tests"""
+
+    def test_current_user_may_already_have_another_membership(self):
+        pass
+
+    """Extra functions"""
+
+    def _assert_membership_is_valid(self):
+        try:
+            self.member.full_clean()
+        except (ValidationError):
+            self.fail('Test member should be valid')
+
+    def _assert_membership_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.member.full_clean()
+
+    def _create_second_member(self, otherUser, otherClub):
+        second_member = Member.objects.create(
+            user_type=UserTypes.MEMBER,
+            current_user=otherUser,
+            club_membership=otherClub
+        )
+        return second_member
+
+    def _create_second_user(self):
+        second_user = User.objects.create_user(
+            username='janedoe@example.org',
+            first_name='Jane',
+            last_name='Doe',
+            password='password123',
+            bio='The quick brown fox jumps over the lazy dog.',
+            personal_statement='hhhh',
+            chess_experience=300,
+        )
+        return second_user
+
+    def _create_second_club(self):
+        second_club = Club.objects.create(
+            name = "Club2",
+            location = "Location2",
+            description = "Description2"
+        )
+        return second_club
